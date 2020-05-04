893f923c5c658345427b851e1e2bba142d38cd24
nzvyagintsev <n.zvyafintsev@gmail.com>
2020-05-04 21:14:31 +0300

    Test refactoring.

--- src/test/java/org/nornis/lightperceptron/TrainingTest.java
+++ src/test/java/org/nornis/lightperceptron/TrainingTest.java
@@ -5,31 +5,6 @@ import org.junit.Test;
 import java.util.Random;
 
 public class TrainingTest {
-    private final double[][] DATA_ANIMALS = {
-            {2, 6, 0, 1, 0, 0, 0, 0}, // insect
-            {2, 2, 1, 0, 1, 0, 0, 0}, // bird
-            {0, 0, 1, 0, 0, 1, 0, 0}, // snake
-            {0, 4, 0, 0, 0, 0, 1, 0}, // mammal
-            {0, 8, 0, 0, 0, 0, 0, 1}  // spider
-
-    };
-
-    @Test
-    public void animalsClassification1() {
-        PerceptronBuilder pb = new PerceptronBuilder();
-        Perceptron p = pb.
-                addLayer(3, 10).
-                addLayer(10, 10).
-                addLayer(10, 5).
-                build();
-
-        p.training(DATA_ANIMALS, 3, 5, 10000);
-        double[] result = p.classification(new double[] {0, 4, 1});
-        assert result[3] != 0;
-
-        result = p.classification(new double[] {2, 6, 0});
-        assert result[0] != 0;
-    }
 
     @Test
     public void approximation() {
@@ -43,15 +18,16 @@ public class TrainingTest {
         Random r = new Random();
         double a, b;
 
+        double [][] data = new double[maxIteration][];
         for (int i = 0; i < maxIteration; i++) {
-            // generate a, b, 0 <= a <= 1, 0 <= a <=
+            // generate a, b, 0 <= a <= 1, 0 <= a <= 1
             a = r.nextDouble();
             b = r.nextDouble();
-            p.training(
-                    new double[]{a, b}, new double[]{Math.sin(a) * Math.cos(b)});
+            data[i] = new double[]{a, b, Math.sin(a) * Math.cos(b)};
         }
+        p.learning(data, 1000);
         a = r.nextDouble();
         b = r.nextDouble();
-        double[] calcResult = p.calculate(new double[] {a, b});
+        double[] calcResult = p.calculateOutput(new double[] {a, b});
     }
 }
