93412a68e452f26ddc4df88a750cf731ce9780d3
nzvyagintsev <n.zvyafintsev@gmail.com>
2020-05-07 13:45:44 +0300

    Added hyperbolic tangent

--- src/main/java/org/nornis/lightperceptron/activators/ActivationFunctionType.java
+++ src/main/java/org/nornis/lightperceptron/activators/ActivationFunctionType.java
@@ -1,11 +1,13 @@
 package org.nornis.lightperceptron.activators;
 
 public enum ActivationFunctionType {
-    SIGMOID;
+    SIGMOID, TANH;
 
     public static IActivationFunction getFunction(ActivationFunctionType type) {
         if(type == SIGMOID)
             return new Sigmoid();
+        if (type == TANH)
+            return new Tanh();
         throw new IllegalArgumentException("Unknown function");
     }
 }
